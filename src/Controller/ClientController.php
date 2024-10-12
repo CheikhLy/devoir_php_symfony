d22c78b8bcdd22d9170e4efa1346df36551f0a10
Cheikh Ly <lyibnoul2@gmail.com>
2024-10-12 15:07:44 +0100

    creation project

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class ClientController extends AbstractController
+{
+    /**
+     * @Route("/client", name="app_client")
+     */
+    public function index(Request $request): Response
+    {
+        return $this->render('client/index.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+}
